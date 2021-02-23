4f9ee4f382e610e623077607128f65869c83f57e
Ayaon-Norhani <ayaon.na27@s.msumain.edu.ph>
2021-02-23 23:54:08 +0800

    initialize an empty project

--- AdmissionProcess.java
+++ AdmissionProcess.java
@@ -2,7 +2,8 @@ public class AdmissionProcess {
     
     public static void main(String[] args){
        
-        System.out.println("Hello There! Welcome to GitHub");
+        System.out.println("1) Initialize an Empty Project");
     
+
         }
     }
