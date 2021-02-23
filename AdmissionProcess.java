7ce9415204bf90da1f14259bcab70a7f6c672745
Ayaon-Norhani <71630199+Ayaon-Norhani@users.noreply.github.com>
2021-02-24 00:13:36 +0800

    This fixes #1

--- AdmissionProcess.java
+++ AdmissionProcess.java
@@ -3,7 +3,7 @@ public class AdmissionProcess {
     public static void main(String[] args){
        
         System.out.println("1) Initialize an Empty Project");
-        System.out.println("DONE!");
+        System.out.println("The number 1 issue is done. Alhamdulillah!");
 
         }
     }
