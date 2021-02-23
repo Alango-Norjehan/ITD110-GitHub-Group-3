66d76c072144334abca641ed749f985d479be0a7
Ayaon-Norhani <71630199+Ayaon-Norhani@users.noreply.github.com>
2021-02-24 00:08:54 +0800

    I have initialize an empty project based on #1

--- AdmissionProcess.java
+++ AdmissionProcess.java
@@ -3,7 +3,7 @@ public class AdmissionProcess {
     public static void main(String[] args){
        
         System.out.println("1) Initialize an Empty Project");
-    
+        System.out.println("DONE!");
 
         }
     }
